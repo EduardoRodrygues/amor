ebc9c138f2e329e03e507ead66585ff3fe5a4f77
EduardoRodrygues <132461396+EduardoRodrygues@users.noreply.github.com>
2025-05-29 13:42:02 -0300

    imagens e musicas

--- site.js
+++ site.js
@@ -1,21 +1,27 @@
 // Array com o conteúdo das cartinhas
 const cartinhasTextos = [
-  { titulo: "Mês 1 - O Início de Tudo", mensagem: "Foi muita sorte a gente ter se conhecido na quela semana de carnaval, e desde então tudo foi incrível. Viramos amigos, melhores amigos, ficamos gays um com o outro e começamos a namorar.", mensagem2: "Te pedir em namoro em 29/06 foi uma das melhores decisões da minha vida. Desde então, vivemos momentos únicos, com abraços, risadas e beijos que dizem tudo" },
+  { titulo: "Mês 1 - O Início de Tudo", mensagem: "Foi muita sorte a gente ter se conhecido naquela semana de carnaval, e desde então tudo foi incrível. Viramos amigos, melhores amigos, ficamos gays um com o outro e começamos a namorar.", mensagem2: "Te pedir em namoro em 29/06 foi uma das melhores decisões da minha vida. Desde então, vivemos momentos únicos, com abraços, risadas e beijos que dizem tudo." },
 
-  { titulo: "Mês 2 - Dois Meses de Nós", mensagem: "Dois meses ao seu lado e só consigo sentir gratidão. Cada momento que nós passamos juntos — as brincadeiras, o love-love, as pregações, é tudo muito especial pra mim. Mesmo com alguns conflitos, que na verdade, só fortaleceram a gente, e me fizeram ver ainda mais o quanto você é importante.",  mensagem2: "Teve o dia que quis ficar sozinho, e percebi que, não quero ficar longe de você, porque sua presença virou essencial para mim. Você me mostrou o que é amar e ser amado, e sou muito grato por isso."},
+  { titulo: "Mês 2 - Dois Meses de Nós", mensagem: "Dois meses ao seu lado e só consigo sentir gratidão. Cada momento que nós passamos juntos — as brincadeiras, o love-love, as pegações, é tudo muito especial pra mim. Mesmo com alguns conflitos, que na verdade, só fortaleceram a gente, e me fizeram ver ainda mais o quanto você é importante.",  mensagem2: "Teve o dia que quis ficar sozinho e percebi que não quero ficar longe de você, porque sua presença virou essencial para mim. Você me mostrou o que é amar e ser amado, e sou muito grato por isso."},
 
-  { titulo: "Mês 3 - A Nossa Conexão Natural ", mensagem: "No terceiro mês, eu amei como tudo entre a gente foi acontecendo de forma natural, nossa intimidade crescendo no nosso tempo, ficamos mais confortáveis um com outro. E percebi que não conseguiria mais imaginar meus dias sem você, sem escutar sua linda voz com seus áudios gigantes (que eu adoro).",  mensagem2: "Obrigado por ser essa pessoa melosa, grudendinha e maravilhosa que eu amo tanto. E claro, que a gente continue sendo dois bobos apaixonados, com muitos beijos, abraços, pregações e, principalmente, amor."},
+  { titulo: "Mês 3 - A Nossa Conexão Natural ", mensagem: "No terceiro mês, eu amei como tudo entre a gente foi acontecendo de forma natural, nossa intimidade crescendo no nosso tempo, ficamos mais confortáveis um com o outro. E percebi que não conseguiria mais imaginar meus dias sem você, sem escutar sua linda voz com seus áudios gigantes (que eu adoro).",  mensagem2: "Obrigado por ser essa pessoa melosa, grudendinha e maravilhosa que eu amo tanto. E claro, que a gente continue sendo dois bobos apaixonados, com muitos beijos, abraços, pregações e, principalmente, amor."},
 
-  { titulo: "Mês 4 - Um Passo de Cada Vez ", mensagem: "Mais um mês dessa história incrível que a gente tá construindo. Mesmo com alguns momentos tristes, a gente sempre se resolve e transforma tudo em risada e leveza. Esse mês foi cheio de brincadeiras, competições que acabamos nós perdendo um pouco(rsrs) e provas de que estamos juntos em tudo, nos momentos bons e ruins.",  mensagem2: "Cada dia só reforça o quanto quero você ao meu lado. E claro, o presente (uma havaiana simples, mas cheia de amor e carinho) é só um lembrete de todos os passos lindos que demos para chegar onde chegamos juntos."},
+  { titulo: "Mês 4 - Um Passo de Cada Vez ", mensagem: "Mais um mês dessa história incrível que a gente tá construindo. Mesmo com alguns momentos tristes, a gente sempre se resolve e transforma tudo em risada e leveza. Esse mês foi cheio de brincadeiras, competições que acabamos nos perdendo um pouco(rsrs) e provas de que estamos juntos em tudo, nos momentos bons e ruins.",  mensagem2: "Cada dia só reforça o quanto quero você ao meu lado. E claro, o presente (uma havaiana simples, mas cheia de amor e carinho) é só um lembrete de todos os passos lindos que demos para chegar onde chegamos juntos."},
 
-  { titulo: "Mês 5 - ", mensagem: "Quando completamos 5 meses foi surreal como quele mês foi incrível e como passou rápido, como as nossas alianças me fizeram ainda mais feliz e realizado, porque elas simbolisão muito coisa, o carinho, repeito, confiança, proximidade, intimidade, união e amor que temos um pelo outro.",  mensagem2: "Esse mês foi cheio de momentos especiais, mas o que mais marcou foi como começamos a nos perder legal. Os Dates trouxeram  tanta felicidade e amor, foram tão especiais para mim, foram momentos que vou guardar no coração para sempre."},
+  { titulo: "Mês 5 - Aliançados e Apaixonados", mensagem: "Quando completamos 5 meses foi surreal como aquele mês foi incrível e como passou rápido, como as nossas alianças me fizeram ainda mais feliz e realizado, porque elas simbolizam muita coisa, o carinho, respeito, confiança, proximidade, intimidade, união e amor que temos um pelo outro.",  mensagem2: "Esse mês foi cheio de momentos especiais, mas o que mais marcou foi como começamos a nos perder legal. Os Dates trouxeram  tanta felicidade e amor, foram tão especiais para mim, foram momentos que vou guardar no coração para sempre."},
 
-  { titulo: "Mês 6 - Presente de Natal", mensagem: "O nosso primeiro Natal juntos, que lindo foi!" },
-  { titulo: "Mês 7 - Teu Aniversário", mensagem: "Comemorar ao seu lado foi especial demais." },
-  { titulo: "Mês 8 - Aprendizados", mensagem: "Cada conversa, cada reconciliação, só nos uniu." },
-  { titulo: "Mês 9 - Mais Conexão", mensagem: "Nossa sintonia ficou ainda mais forte." },
-  { titulo: "Mês 10 - Planos Juntos", mensagem: "Pensar no futuro ao teu lado é maravilhoso." },
+  { titulo: "Mês 6 - Longe, Mas Próximos", mensagem: "Esse mês, você ficou alguns dias na roça, e esses dias, só me fizeram perceber o quanto eu amo sua presença, senti muito a sua falta. Senti uma saudade gigante nesses dias, porque eu não tinha a sua companhia para tornar o meu dia melhor, mas quando a gente conseguiu se falar de novo, foi maravilhoso, tivemos callzinhas, e cada segundo valeu a espera.",  mensagem2: "A resenha com seus amigos foi legal, até curti fazer o slide no fim (apesar das reclamações), foi divertido, por mais que eu tenha sido um pouco chato."},
+
+  { titulo: "Mês 7 - Mais Que Férias, Amor", mensagem: "Esse sétimo mês foi muito pika, porque a gente passou muito mais tempo juntinhos, curtindo as férias e aproveitando a companhia um do outro. Ter você no meu aniversário foi o melhor presente que eu poderia querer.",  mensagem2: "O presente incrível que você fez foi incrível e me mostrou, mais uma vez, o quanto você se importa comigo. Esses dias juntos foram especiais, poder passar várias horas com você, rir, conversar, aproveitar o tempo sem preocupações ... foi muito bom."},
+
+  { titulo: "Mês 8 - Eu  Erreeeeiiiiii", mensagem: "Nesse oitavo mês, a gente acabou discutindo um pouco, mas conversamos, entendemos um ao outro e encontramos um jeito de resolver as coisas, o que eu adoro no nosso relacionamento, que nunca deixamos isso nos abalar e sempre e ficamos bem. ",  mensagem2: "Acabei sendo bem negligente com você meu amor, não te dando o cuidado e a atenção que você merece, me desculpa amor. Não foi um dos melhores meses, mas conseguimos supera-lo e continuamos crescendo juntos."},
+  
+  { titulo: "Mês 9 - Desculpas e Gratidão", mensagem: "Nesse nono mês, percebi o quanto já vivemos juntos — risadas, desafios e muito amor. Sei que em alguns momentos eu fiquei meio distante, preso nos meus pensamentos e não me abrindo com você, e acabei fazendo merda, e não demonstrei o carinho que você merece, me desculpa por ter te causado alguma insegurança, Mas saiba que te amo demais e nunca quis te deixar insegura.",  mensagem2: "Tenho que te agradecer também, porque seu cuidado comigo, tipo quando fiquei fodido de dor nas costas,  você tirou um tempo para me ajudar, foi um gesto tão fofo, que significou muito para mim. Só me faz ter mais certeza de que estar com você é a melhor coisa que me aconteceu."},
+
+  { titulo: "Mês 10 - Novas Experiências e Amor", mensagem: "Esse décimo mês foi diferente de todos os outros. Toda vez que a gente se encontrou, fizemos algo novo e fora da rotina — fritamos batata, fizemos ovos de Páscoa, preparamos hambúrguer… E, qualquer coisa com você se torna incrível. Foi pika viver essas experiências simples, mas cheias de significado, do seu lado.",  mensagem2: "Foi mal ter escondido aquele problema de você por tanto tempo. Eu devia ter confiado mais em nós dois e te contado antes. Mas fico muito feliz que, mesmo com isso, a gente conseguiu conversar e resolver tudo de uma forma simples, mostrando que o problema não era tão grande quanto eu imaginava."},
+
   { titulo: "Mês 11 - Maturidade", mensagem: "Crescemos tanto juntos nesse mês." },
+
   { titulo: "Mês 12 - 1 Ano de Amor", mensagem: "Chegamos até aqui e quero mais mil anos contigo." }
 ];
 
@@ -23,11 +29,58 @@ const cartinhasTextos = [
 // Troca de imagem a cada 10 segundos
 const imagens = document.querySelectorAll(".image");
 let indice = 0;
-setInterval(() => {
-  imagens[indice].classList.remove("active");
+let intervalo;
+
+function mostrarImagem(index) {
+  imagens.forEach(img => img.classList.remove("active"));
+  imagens[index].classList.add("active");
+}
+
+function proximaImagem() {
   indice = (indice + 1) % imagens.length;
-  imagens[indice].classList.add("active");
-}, 10000);
+  mostrarImagem(indice);
+}
+
+function imagemAnterior() {
+  indice = (indice - 1 + imagens.length) % imagens.length;
+  mostrarImagem(indice);
+}
+
+// Função para resetar o intervalo automático
+function resetInterval() {
+  clearInterval(intervalo);
+  intervalo = setInterval(proximaImagem, 10000);
+}
+
+// Inicia o intervalo
+resetInterval();
+
+// Detectar deslize no celular
+let touchStartX = 0;
+let touchEndX = 0;
+
+const imageContainer = document.querySelector(".image-container");
+
+imageContainer.addEventListener("touchstart", e => {
+  touchStartX = e.changedTouches[0].screenX;
+});
+
+imageContainer.addEventListener("touchend", e => {
+  touchEndX = e.changedTouches[0].screenX;
+  handleGesture();
+});
+
+function handleGesture() {
+  const distancia = touchEndX - touchStartX;
+
+  if (distancia > 50) {
+    imagemAnterior();
+    resetInterval();
+  } else if (distancia < -50) {
+    proximaImagem();
+    resetInterval();
+  }
+}
 
 const musicas = [
   {
@@ -41,14 +94,19 @@ const musicas = [
     capa: "img/capa2.jpg"
   },
   {
-    titulo: "terceira Música",
+    titulo: "Memories",
     arquivo: "musicas/musica3.mp3",
     capa: "img/capa3.jpg"
   },
   {
-    titulo: "Quarta Música",
+    titulo: "Meu Esquema",
     arquivo: "musicas/musica4.mp3",
     capa: "img/capa4.jpg"
+  },
+  {
+    titulo: "Você Gosta Dela",
+    arquivo: "musicas/musica5.mp3",
+    capa: "img/capa5.jpg"
   }
 ];
 
